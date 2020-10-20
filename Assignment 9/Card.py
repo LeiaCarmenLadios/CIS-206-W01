e6efb6c1d1446819e1473a1669d4288073e77472
l_ladios <49729440+LeiaCarmenLadios@users.noreply.github.com>
2020-10-20 18:43:41 -0500

    Update Card.py

--- Assignment 9/Card.py
+++ Assignment 9/Card.py
@@ -1,20 +1,8 @@
 class Card:
-    card_value = None
-    card_suit = None
-    
     def __init__(self, card_value = None, card_suit = None):
         self.card_value = card_value
         self.card_suit = card_suit
 
-    # @property
-    # def card_value(self):
-    #     """Gets/returns the VALUE"""
-    #     return self._card_value
-
-
-    # @property
-    # def card_suit(self):
-    #     """Gets/returns SUIT value"""
-    #     return self._card_suit
-
-
+    def __str__(self):
+        return "{} of {}".format(self.card_value, self.card_suit)
+    
