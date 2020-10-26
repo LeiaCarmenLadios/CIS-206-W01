7facc702fa90cf5d0175738d9a2f09df6cfc568e
l_ladios <49729440+LeiaCarmenLadios@users.noreply.github.com>
2020-10-26 16:37:55 -0500

    Update Deck.py

--- Assignment 10/Deck.py
+++ Assignment 10/Deck.py
@@ -2,15 +2,19 @@ import Card as Card_library
 import random
 
 class Deck:
-    deck_of_cards = []
-    
+   
+    @property
+    def deck_of_cards(self):
+        return self._deck_of_cards
+
     def __init__(self):
         VALUE = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
         SUIT = ['♧', '♢', '♡', '♤']
+        self._deck_of_cards = []
         for s in SUIT:
             for v in VALUE:
                 crd = Card_library.Card(v, s)
-                self.deck_of_cards.append(crd)
+                self._deck_of_cards.append(crd)
 
     def __str__(self):
         printString = ''
@@ -19,10 +23,10 @@ class Deck:
         return printString 
 
     def shuffle(self):
-       random.shuffle(self.deck_of_cards)
+       return random.shuffle(self.deck_of_cards)
 
     def draw(self):
-        return self.deck_of_cards.pop()
+        return self._deck_of_cards.pop()
 
     def reset(self):
         self.__init__()
@@ -33,6 +37,7 @@ class Deck:
 # print(obj)
 # print("\n")
 # obj.shuffle()
+# print(obj)
 # for c in obj.deck_of_cards:   
 #     print(c.card_suit + c.card_value)
 # obj.reset()
