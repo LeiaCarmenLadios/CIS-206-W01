99b6e6f139c5e4700dc4f8a4c57b987a624af9eb
zbogdanski <54122062+zbogdanski@users.noreply.github.com>
2020-10-26 14:36:50 -0500

    Added back properties

--- Assignment 10/Card.py
+++ Assignment 10/Card.py
@@ -1,7 +1,20 @@
 class Card:
+     @property
+    def card_value(self):
+        """Gets/returns the VALUE"""
+        return self._card_value
+
+    @property
+    def card_suit(self):
+        """Gets/returns SUIT value"""
+        return self._card_suit
+    
     def __init__(self, card_value = None, card_suit = None):
-        self.card_value = card_value
-        self.card_suit = card_suit
+        self._card_value = card_value
+        self._card_suit = card_suit
 
     def __str__(self):
         return "{} of {}".format(self.card_value, self.card_suit)
+    
+    
+   
