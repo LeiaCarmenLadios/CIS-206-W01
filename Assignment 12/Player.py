9c41b1125285fd242c11e487954e444a17b57b72
zbogdanski <54122062+zbogdanski@users.noreply.github.com>
2020-11-10 20:34:43 -0600

    Update Player.py

--- Assignment 12/Player.py
+++ Assignment 12/Player.py
@@ -1,6 +1,7 @@
 import Hand as Hand_library
 import Card as Card_library
 import Deck as Deck_library
+import sys
 
 class Player:
    
@@ -44,14 +45,17 @@ class Player:
         self._player_hand = Hand_library.Hand()
 
     def askForCard(self):
-       
         # print out their hand so that they know what options are available to them
         # They can only ask for a card if they have at least one of that value card
         while True:
             print(self.player_hand)
             askplayer_card = input("Which card would you like to ask for? [card value]: ") # string
-            value_card, suit_card = askplayer_card.split(' of ')
-                # ask_card = Card_library.Card()
+            if askplayer_card == "Quit" or askplayer_card == "quit":
+                sys.exit(0)
+            try:  
+                value_card, suit_card = askplayer_card.split(' of ')
+                if value_card.isalpha():
+                    value_card = value_card[0].upper()
                 if suit_card[0].lower() == 'h' or suit_card == '♡':
                     ask_card = Card_library.Card(value_card, '♡')
                 elif suit_card[0].lower() == 's' or suit_card == '♤':
@@ -62,23 +66,20 @@ class Player:
                     ask_card = Card_library.Card(value_card, '♢')
                 return ask_card
             except:
-                print('Invalid option. Try Again')
+                print('\nInvalid card. Try Again')
                 print('Ex: A of ♧ or A of spade')
               
         # if askplayer_card in self.player_hand:
         #     if askplayer_card in askplayer_player.player_hand:
         #         pass # PLACEHOLDER
         # else:
-        #     pass # PLACEHOLDER
-
-   
-    
+        #     pass # PLACEHOLDER 
 
     def addToScore(self):
-        score += 1
+        self.score += 1
     
     def __str__(self):
-        return "name: {} score: {}\n".format(self.name, self.score)
+        return "\nName: {} Score: {} Books: {}".format(self.name, self.score, self.books)
 
 
 if __name__ == "__main__":
