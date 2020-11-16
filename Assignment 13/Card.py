20985640193a1f096998d7bef0578a3019ee626d
l_ladios <49729440+LeiaCarmenLadios@users.noreply.github.com>
2020-11-15 19:16:12 -0600

    Update Card.py

--- Assignment 13/Card.py
+++ Assignment 13/Card.py
@@ -1,1 +1,29 @@
+import Deck as Deck_library
 
+class Card:
+    @property
+    def card_value(self):
+        """Gets/returns the VALUE"""
+        return self._card_value
+
+    @property
+    def card_suit(self):
+        """Gets/returns SUIT value"""
+        return self._card_suit
+
+    def __init__(self, card_value = None, card_suit = None):
+        VALUE = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        SUIT = ['♧', '♢', '♡', '♤']
+        if (card_value not in VALUE) or (card_suit not in SUIT):
+            raise Exception('ERROR - Invalid card VALUE or SUIT')
+        else:
+            self._card_value = card_value
+            self._card_suit = card_suit  
+
+    def __str__(self):
+        return "{} of {}".format(self.card_value, self.card_suit)
+
+
+if __name__ == '__main__':
+    crd = Card('A', 'F')
+    print(crd)
