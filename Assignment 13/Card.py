749ce7c53c67b8a9a85b0c1d187b1c33060555b1
zbogdanski <54122062+zbogdanski@users.noreply.github.com>
2020-11-17 21:54:39 -0600

    Update Card.py

--- Assignment 13/Card.py
+++ Assignment 13/Card.py
@@ -10,15 +10,21 @@ class Card:
     def card_suit(self):
         """Gets/returns SUIT value"""
         return self._card_suit
+    
+    @property
+    def card_file(self):
+        """Gets/returns card URL value"""
+        return self._card_file
 
-    def __init__(self, card_value = None, card_suit = None):
+    def __init__(self, card_value = None, card_suit = None, card_file = None):
         VALUE = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
         SUIT = ['♧', '♢', '♡', '♤']
         if (card_value not in VALUE) or (card_suit not in SUIT):
             raise Exception('ERROR - Invalid card VALUE or SUIT')
         else:
             self._card_value = card_value
-            self._card_suit = card_suit  
+            self._card_suit = card_suit
+            self._card_file = card_file
 
     def __str__(self):
         return "{} of {}".format(self.card_value, self.card_suit)
@@ -27,3 +33,5 @@ class Card:
 if __name__ == '__main__':
     crd = Card('A', 'F')
     print(crd)
+
+    
