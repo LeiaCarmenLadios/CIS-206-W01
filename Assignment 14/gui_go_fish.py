ed3bc5187f93abee9ef8eb7dd091f9e50b94b416
l_ladios <49729440+LeiaCarmenLadios@users.noreply.github.com>
2020-12-09 18:42:24 -0600

    Update gui_go_fish.py

--- Assignment 14/gui_go_fish.py
+++ Assignment 14/gui_go_fish.py
@@ -56,8 +56,13 @@ class Canvas(tk.Canvas):
         self.display_start_page()
         self.images = []
   
-    def askNumPlayers(self):
-       
+    def askNumPlayers(self, button1 = "", button2 = ""):
+        self.clear_canvas()
+        if(isinstance(button1, tk.Button)):
+            button1.destroy()
+        if(isinstance(button2, tk.Button)):
+            button2.destroy()
+        
         intro_msg = self.create_text(580, 300, font = ("Purisa", 18),
         text = """
         \t        Welcome to the game of GO FISH!!!\n
@@ -93,6 +98,7 @@ class Canvas(tk.Canvas):
             self.askNumPlayers()
         else:
             self.get_names()
+
                 
     def get_names(self):
         entered_names = []
@@ -115,17 +121,23 @@ class Canvas(tk.Canvas):
             
            
 
-        submitPlayerName_button = tk.Button(self, text ="Submit", bg="light cyan", command = lambda:[self.assignPlayerName(entered_names), submitPlayerName_button.destroy()])
+        submitPlayerName_button = tk.Button(self, text ="Submit", bg="light cyan") # lambda:[self.assignPlayerName(entered_names), submitPlayerName_button.destroy()]
+        submitPlayerName_button['command'] = partial(self.assignPlayerName, entered_names, submitPlayerName_button)
         submitPlayerName_button.grid(row = self.game.num_of_players + 5, column = 0, pady =  20, padx =50)
         
-  
-        # for _ in range(self.game.num_of_players):
-            
-        #     enter_name = tk.Entry(self, textvariable = playerNameGiven)
-           
-        #     enter_name.place(x=390, y = 350, width = 200)
-        #    
-            # submitPlayerName_button.wait_variable(playerNameGiven.get())
+    def assignPlayerName(self, playerNames, button):
+        button.destroy()
+        for x in playerNames:
+            self.game.addPlayers(x.get())
+
+        for entryBox in self.grid_slaves():
+            entryBox.grid_forget()
+
+        print(self.game.print_player_list())
+        self.clear_canvas()
+        self.grid_forget()
+        self.displayTurn()
+
             
     def displayTurn(self):  #https://pypi.org/project/unicards/ unicode characters for playing cards
         self.game.deal()    
@@ -223,52 +235,40 @@ class Canvas(tk.Canvas):
             self.printCards()
             self.makeAskForPlayerName()
             
-            
-
-            
-             
-        
-    def assignPlayerName(self, playerNames):
-        for x in playerNames:
-            self.game.addPlayers(x.get())
-
-        for entryBox in self.grid_slaves():
-            entryBox.grid_forget()
-
-        print(self.game.print_player_list())
+           
+    def display_start_page(self, button1 = ""):
         self.clear_canvas()
-        self.grid_forget()
-        self.displayTurn()
-       
-    def display_start_page(self):
-        startGame_button = tk.Button(self, text ="Start Game", font = ("Purisa", 14), bg="light cyan", command =lambda:[self.clear_canvas(), self.askNumPlayers(), startGame_button.destroy(), gameRules_button.destroy()])
+        
+        if(isinstance(button1, tk.Button)):
+            button1.destroy()
+        
+        startGame_button = tk.Button(self, text ="Start Game", font = ("Purisa", 14), bg="light cyan") #lambda:[self.clear_canvas(), self.askNumPlayers(), startGame_button.destroy(), gameRules_button.destroy()]
         startGame_button.pack(pady = (300, 20))
-        gameRules_button = tk.Button(self, text ="Game Rules", font = ("Purisa", 14), bg="light cyan", command =lambda:[self.clear_canvas(), self.display_game_rules(), gameRules_button.destroy(), startGame_button.destroy()] )
+        gameRules_button = tk.Button(self, text ="Game Rules", font = ("Purisa", 14), bg="light cyan")
         gameRules_button.pack()
+        startGame_button['command'] = partial(self.askNumPlayers, startGame_button, gameRules_button)
+        gameRules_button['command'] = partial(self.display_game_rules, gameRules_button, startGame_button)
         
     def clear_canvas(self):
         self.delete("all")
-        
-    def retrieve_canvas(self):
-        self.clear_canvas()
 
-    def display_game_rules(self):
-        self.create_text(580, 350, font=("Purisa", 12), 
+
+    def display_game_rules(self, button1, button2):
+        self.clear_canvas()
+        button1.destroy()
+        button2.destroy()
+        self.create_text(580, 370, font=("Purisa", 13), 
         text=  """
         GAME RULES-
         
-        1>  Any player deals one card face up to each player. The player with the lowest card is the dealer. 
-            The dealer shuffles the cards, and the player to the right cuts them.
-
-        2>  The dealer completes the cut and deals the cards clockwise one at a time, face down, beginning with 
-            the player to the left. Maximum number of players is 5 and everyone is dealt 5 cards each. The remainder 
+        1>  Each player is dealt five cards at the beginning of the game/  The remainder 
             of the pack is placed face down on the table to form the stock.
 
-        3>  The player to the left of the dealer looks directly at any opponent and says, for example, "Give me your 
-            kings," usually addressing the opponent by name and specifying the rank that they want, from ace down to two.
+        2>  The first player whose name was entered first, goes first in the round of players. 
 
-        4>  The player who is "fishing “must have at least one card of the rank that was asked for in their hand. The 
-            player who is addressed must hand over all the cards requested.
+        3>  The player whose turn it is will be prompted to choose another player to ask a card from.
+
+        4>  The player whose turnit is will then choose a card value they would like to ask that player for.
 
         5>  If the player has none, they say, "Go fish!" and the player who made the request draws the top card of the 
             deck and places it in their hand.
@@ -291,94 +291,11 @@ class Canvas(tk.Canvas):
 
         11>The game ends when all thirteen books have been won. The winner is the player with the most books.""")
 
-        back_button = tk.Button(self, text = "Back", bg="light cyan", command = lambda:[self.retrieve_canvas(), back_button.destroy(), self.display_start_page()])
+        back_button = tk.Button(self, text = "Back", bg="light cyan", font=("Purisa", 12))
         back_button.pack()
+        back_button['command'] = partial(self.display_start_page, back_button)
         
 
-        
-        
-
-
-# root = tk.Tk()
-# root.title("GO FISH!")
-
-# # Frames
-# main_frame = tk.Frame(root)
-# GameRules_frame = tk.Frame(root)
-# TheGame_frame = tk.Frame(root)
-
-
-    
-# def game_rules():
-#     main_frame.pack_forget()
-#     GameRules_frame.pack()
-#     # GameRules_frame.tkraise()
-
-# def show_main_frame():
-#     GameRules_frame.pack_forget()
-#     main_frame.pack()
-
-
-# description_1 = tk.Label(
-#     master=main_frame,
-#     text="Go Fish Game",
-#     foreground="white",  # Set the text color to white
-#     background="black"  # Set the background color to black
-# )
-
-# button_StartGame = tk.Button(
-#     master=main_frame,
-#     text="Start Game",
-#     #width=25,
-#     #height=3,
-#     #bg="blue",
-#     #fg="yellow",
-#     padx = 100, 
-#     pady= 10,
-#     command = new_game
-# )
-# #button_StartGame.grid(row = 0, column = 0)
-# button_StartGame.place(x = 336 , y = 325)
-# button_StartGame.pack()
-
-# button_GameRules = tk.Button(
-#     master=main_frame,
-#     text="Game Rules",
-#     #width=25,
-#     #height=3,
-#     #bg="blue",
-#     #fg="yellow",
-#     padx = 100,
-#     pady = 10,
-#     command = game_rules
-# )
-# # button_GameRules.grid(row = 1, column = 0)
-# button_GameRules.place(x = 335, y = 385)
-# button_GameRules.pack()
-
-
-
-# button_GameRules_back = tk.Button(
-#     master=GameRules_frame,
-#     text="Go Back",
-#     #width=25,
-#     #height=3,
-#     #bg="blue",
-#     #fg="yellow",
-#     padx = 100,
-#     pady = 10,
-#     command = show_main_frame
-# )
-# # button_GameRules.grid(row = 1, column = 0)
-# button_GameRules_back.pack()
-
-# main_frame.pack()
-
-# # button.grid(row = 0, column = 0)
-# # button_GameRules.grid(row = 1, column = 0)
-
-# # text_box = tk.Text(root, height = 4, width = 50)
-# # text_box.pack(side = tk.BOTTOM)
 
 if __name__ == "__main__":
     root = Root()
