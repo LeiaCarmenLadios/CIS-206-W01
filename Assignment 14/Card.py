2039ad7a89a80c93971578cd24acc141ef58891f
Jacob <54375291+jacobkazmierczak@users.noreply.github.com>
2020-12-11 05:29:00 -0600

    Added documentation.

--- Assignment 14/Card.py
+++ Assignment 14/Card.py
@@ -1,6 +1,7 @@
 import Deck as Deck_library
 
 class Card:
+    """Sets card values and prints them out."""
     @property
     def card_value(self):
         """Gets/returns the VALUE"""
@@ -17,6 +18,18 @@ class Card:
         return self._card_file
 
     def __init__(self, card_value = None, card_suit = None, card_file = None):
+        """Checks card attributes in list for match (data validation).
+            If matched, it will set the corresponding card data values. 
+        
+        Args:
+            card_value (string): Value of the card.
+            card_suit (string): Suit of the card.
+            card_file (string): URL of the card image.
+
+        Raises:
+            Exception: If card_value or card_suit is not in the list.
+
+        """
         VALUE = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
         SUIT = ['♧', '♢', '♡', '♤']
         if (card_value not in VALUE) or (card_suit not in SUIT):
@@ -27,10 +40,14 @@ class Card:
             self._card_file = card_file
 
     def __str__(self):
+        """__str__ called when using print().
+            Prints out card information in certain format.
+        """
         return "{} of {}".format(self.card_value, self.card_suit)
 
 
 if __name__ == '__main__':
+    """Sets card values and prints them out."""
     crd = Card('A', 'F')
     print(crd)
 
